d2a244ca4b9daadd06e1ae749ba9f6db0a8c174b
krawieck <fil.krawczyk@gmail.com>
2019-01-23 22:42:57 +0100

    moved some error handling to separate function

--- main.js
+++ main.js
@@ -11,39 +11,42 @@ const url = argv._[0]
 const width = Number(argv.w) || 1920
 const height = Number(argv.h) || 1080
 const headless = !argv.headful
-const scrollFirst = argv['scrol-first'] || false
+const scrollFirst = argv['scroll-first'] || false
 let o = argv.o || `webpage_screenshot_${new Date().toLocaleString('pl')}.png`
 
 const output = o.endsWith('.png') || o.endsWith('.jpg') || o.endsWith('.jpeg') ? o : `${o}.png`
 
 const args = argv['linux-workaround'] ? ['--no-sandbox', '--disable-setuid-sandbox'] : []
 ;(async () => {
-	const browser = await puppeteer.launch({ args, headless }).catch(e => {
-		console.log(
-			e +
-				'======================================\n' +
-				"seems like somethig went wrong when launching chromium, if you're on " +
-				'linux you might wanna try running this tool with "--linux-workaround" flag, ' +
-				'but BEWARE, IT DISABLES SANDBOXING WHICH MIGHT LEAD TO SOME BAD STUFF WITH ' +
-				"SOME DODGY WEBSITES\n\noh, also read the puppeteer's error message above"
+	const browser = await puppeteer
+		.launch({ args, headless })
+		.catch(e =>
+			errorExit(
+				e +
+					'======================================\n' +
+					"seems like somethig went wrong when launching chromium, if you're on " +
+					'linux you might wanna try running this tool with "--linux-workaround" flag, ' +
+					'but BEWARE, IT DISABLES SANDBOXING WHICH MIGHT LEAD TO SOME BAD STUFF WITH ' +
+					"SOME DODGY WEBSITES\n\noh, also read the puppeteer's error message above"
+			)
 		)
-		process.exit()
-	})
-	const page = await browser.newPage().catch(e => {
-		console.log(
-			"couln't open new page, here's the error msg, if u wanna figure out what went wrong:\n",
-			e
+	const page = await browser
+		.newPage()
+		.catch(e =>
+			errorExit(
+				"couln't open new page, here's the error msg, if u wanna figure out what went wrong:\n",
+				e
+			)
 		)
-		process.exit(1)
-	})
-	await page.goto(url, { waitUntil: 'networkidle0' }).catch(e => {
-		console.log(
-			"couldn't go to desired page, check the url and make sure it has the 'http' part, also here's the puppeteer's error message:",
-			e
+	await page
+		.goto(url, { waitUntil: 'networkidle2' })
+		.catch(e =>
+			errorExit(
+				"couldn't go to desired page, check the url and make sure it has the 'http' part, also here's the puppeteer's error message:",
+				e
+			)
 		)
-		process.exit(1)
-	})
-  await page.setViewport({ width, height })
+	await page.setViewport({ width, height })
 
 	if (scrollFirst) {
 		await page.evaluate(
@@ -53,7 +56,7 @@ const args = argv['linux-workaround'] ? ['--no-sandbox', '--disable-setuid-sandb
 						const maxScroll = Number.MAX_SAFE_INTEGER
 						let lastScroll = 0
 						const interval = setInterval(() => {
-							window.scrollBy(0, 100)
+							window.scrollBy(0, 200)
 							const scrollTop = document.documentElement.scrollTop
 							if (scrollTop === maxScroll || scrollTop === lastScroll) {
 								clearInterval(interval)
@@ -69,10 +72,14 @@ const args = argv['linux-workaround'] ? ['--no-sandbox', '--disable-setuid-sandb
 		)
 	}
 
-	await page.screenshot({ path: output, fullPage: true }).catch(e => {
-		console.log("couldn't take screenshot, here's puppeteer's error msg:\n", e)
-		process.exit(1)
-	})
+	await page
+		.screenshot({ path: output, fullPage: true })
+		.catch(e => errorExit("couldn't take screenshot, here's puppeteer's error msg:\n", e))
 
 	await browser.close()
 })()
+
+function errorExit(...e) {
+	console.log(...e)
+	process.exit(1)
+}
