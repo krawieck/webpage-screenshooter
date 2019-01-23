292aa1c9828219df393b35071db215f1c128bc49
krawieck <fil.krawczyk@gmail.com>
2019-01-23 23:01:15 +0100

    cleanup, added some comments

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const puppeteer = require('puppeteer')
 const argv = require('minimist')(process.argv.slice(2))
 if (argv.verbose) console.log(argv)
 
+// Parse args
 if (!argv._[0]) {
   console.log('url as positional argument is required!')
   process.exit(1)
@@ -17,38 +18,45 @@ let o = argv.o || `webpage_screenshot_${new Date().toLocaleString('pl')}.png`
 const output = o.endsWith('.png') || o.endsWith('.jpg') || o.endsWith('.jpeg') ? o : `${o}.png`
 
 const args = argv['linux-workaround'] ? ['--no-sandbox', '--disable-setuid-sandbox'] : []
+
+// Do the screenshooting
 ;(async () => {
   const browser = await puppeteer
     .launch({ args, headless })
     .catch(e =>
       errorExit(
-        e +
-          '======================================\n' +
-          "seems like somethig went wrong when launching chromium, if you're on " +
-          'linux you might wanna try running this tool with "--linux-workaround" flag, ' +
-          'but BEWARE, IT DISABLES SANDBOXING WHICH MIGHT LEAD TO SOME BAD STUFF WITH ' +
-          "SOME DODGY WEBSITES\n\noh, also read the puppeteer's error message above"
+        e + '======================================',
+        "\nseems like somethig went wrong when launching chromium, if you're on",
+        'linux you might wanna try running this tool with "--linux-workaround" flag,',
+        'but BEWARE, IT DISABLES SANDBOXING WHICH MIGHT LEAD TO SOME BAD STUFF WITH',
+        "SOME DODGY WEBSITES\n\noh, also read the puppeteer's error message above"
       )
     )
   const page = await browser
     .newPage()
     .catch(e =>
       errorExit(
-        "couln't open new page, here's the error msg, if u wanna figure out what went wrong:\n",
+        "couln't open new page, here's the error msg,",
+        'if u wanna figure out what went wrong:\n',
         e
       )
     )
+
   await page
     .goto(url, { waitUntil: 'networkidle2' })
     .catch(e =>
       errorExit(
-        "couldn't go to desired page, check the url and make sure it has the 'http' part, also here's the puppeteer's error message:",
+        "couldn't go to desired page, check the url",
+        "and make sure it has the 'http' part, also",
+        "here's the puppeteer's error message:",
         e
       )
     )
+
   await page.setViewport({ width, height })
 
   if (scrollFirst) {
+    // scrolls through page to make sure everything has loaded
     await page.evaluate(
       async () =>
         new Promise((resolve, reject) => {
@@ -82,4 +90,18 @@ const args = argv['linux-workaround'] ? ['--no-sandbox', '--disable-setuid-sandb
 function errorExit(...e) {
   console.log(...e)
   process.exit(1)
+}
+
+function getFormatedDate() {
+  const date = new Date()
+  return `${date.getFullYear()}-${pad(date.getMonth())}-${pad(date.getDate())}\
+_${date.getHours()}-${date.getMinutes()}-${date.getSeconds()}`
+}
+
+function pad(str, num = 2) {
+  let out = String(str)
+  while (out.length < num) {
+    out = `0${out}`
+  }
+  return out
 }
