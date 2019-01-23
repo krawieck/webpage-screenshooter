956f3843be78c87420634efdf00347495ee83c48
krawieck <fil.krawczyk@gmail.com>
2019-01-23 22:45:19 +0100

    .prettierrc changes

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const argv = require('minimist')(process.argv.slice(2))
 if (argv.verbose) console.log(argv)
 
 if (!argv._[0]) {
-	console.log('url as positional argument is required!')
-	process.exit(1)
+  console.log('url as positional argument is required!')
+  process.exit(1)
 }
 
 const url = argv._[0]
@@ -18,68 +18,68 @@ const output = o.endsWith('.png') || o.endsWith('.jpg') || o.endsWith('.jpeg') ?
 
 const args = argv['linux-workaround'] ? ['--no-sandbox', '--disable-setuid-sandbox'] : []
 ;(async () => {
-	const browser = await puppeteer
-		.launch({ args, headless })
-		.catch(e =>
-			errorExit(
-				e +
-					'======================================\n' +
-					"seems like somethig went wrong when launching chromium, if you're on " +
-					'linux you might wanna try running this tool with "--linux-workaround" flag, ' +
-					'but BEWARE, IT DISABLES SANDBOXING WHICH MIGHT LEAD TO SOME BAD STUFF WITH ' +
-					"SOME DODGY WEBSITES\n\noh, also read the puppeteer's error message above"
-			)
-		)
-	const page = await browser
-		.newPage()
-		.catch(e =>
-			errorExit(
-				"couln't open new page, here's the error msg, if u wanna figure out what went wrong:\n",
-				e
-			)
-		)
-	await page
-		.goto(url, { waitUntil: 'networkidle2' })
-		.catch(e =>
-			errorExit(
-				"couldn't go to desired page, check the url and make sure it has the 'http' part, also here's the puppeteer's error message:",
-				e
-			)
-		)
-	await page.setViewport({ width, height })
+  const browser = await puppeteer
+    .launch({ args, headless })
+    .catch(e =>
+      errorExit(
+        e +
+          '======================================\n' +
+          "seems like somethig went wrong when launching chromium, if you're on " +
+          'linux you might wanna try running this tool with "--linux-workaround" flag, ' +
+          'but BEWARE, IT DISABLES SANDBOXING WHICH MIGHT LEAD TO SOME BAD STUFF WITH ' +
+          "SOME DODGY WEBSITES\n\noh, also read the puppeteer's error message above"
+      )
+    )
+  const page = await browser
+    .newPage()
+    .catch(e =>
+      errorExit(
+        "couln't open new page, here's the error msg, if u wanna figure out what went wrong:\n",
+        e
+      )
+    )
+  await page
+    .goto(url, { waitUntil: 'networkidle2' })
+    .catch(e =>
+      errorExit(
+        "couldn't go to desired page, check the url and make sure it has the 'http' part, also here's the puppeteer's error message:",
+        e
+      )
+    )
+  await page.setViewport({ width, height })
 
-	if (scrollFirst) {
-		await page.evaluate(
-			async () =>
-				new Promise((resolve, reject) => {
-					try {
-						const maxScroll = Number.MAX_SAFE_INTEGER
-						let lastScroll = 0
-						const interval = setInterval(() => {
-							window.scrollBy(0, 200)
-							const scrollTop = document.documentElement.scrollTop
-							if (scrollTop === maxScroll || scrollTop === lastScroll) {
-								clearInterval(interval)
-								resolve()
-							} else {
-								lastScroll = scrollTop
-							}
-						}, 100)
-					} catch (err) {
-						reject(err)
-					}
-				})
-		)
-	}
+  if (scrollFirst) {
+    await page.evaluate(
+      async () =>
+        new Promise((resolve, reject) => {
+          try {
+            const maxScroll = Number.MAX_SAFE_INTEGER
+            let lastScroll = 0
+            const interval = setInterval(() => {
+              window.scrollBy(0, 200)
+              const scrollTop = document.documentElement.scrollTop
+              if (scrollTop === maxScroll || scrollTop === lastScroll) {
+                clearInterval(interval)
+                resolve()
+              } else {
+                lastScroll = scrollTop
+              }
+            }, 100)
+          } catch (err) {
+            reject(err)
+          }
+        })
+    )
+  }
 
-	await page
-		.screenshot({ path: output, fullPage: true })
-		.catch(e => errorExit("couldn't take screenshot, here's puppeteer's error msg:\n", e))
+  await page
+    .screenshot({ path: output, fullPage: true })
+    .catch(e => errorExit("couldn't take screenshot, here's puppeteer's error msg:\n", e))
 
-	await browser.close()
+  await browser.close()
 })()
 
 function errorExit(...e) {
-	console.log(...e)
-	process.exit(1)
+  console.log(...e)
+  process.exit(1)
 }
